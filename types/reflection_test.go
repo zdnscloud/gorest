1bdcab3a95d3917ff5869dd586050643f4ec5dae
joe-yu <yuhaijiao@zdns.cn>
2019-03-04 11:26:40 +0000

    add func for get resource type name

--- types/reflection_test.go
+++ types/reflection_test.go
@@ -12,6 +12,11 @@ var version = APIVersion{
 	Path:    "/v1",
 }
 
+type Cluster struct {
+	Resource
+	Name string `singlecloud:"notnullable"`
+}
+
 type Node struct {
 	Resource
 	Name string `singlecloud:"notnullable"`
@@ -20,17 +25,17 @@ type Node struct {
 func TestReflection(t *testing.T) {
 	schemas := NewSchemas()
 	schemas.MustImportAndCustomize(&version, Node{}, nil, func(schema *Schema, handler Handler) {
-		schema.Parent = "cluster"
+		schema.Parent = GetTypeName(Cluster{})
 		schema.CollectionMethods = []string{"GET", "POST"}
 		schema.ResourceMethods = []string{"GET", "DELETE", "PUT"}
 	})
 
-	schema := schemas.Schema(&version, "node")
-	ut.Equal(t, schema.ID, "node")
+	schema := schemas.Schema(&version, GetTypeName(Node{}))
+	ut.Equal(t, schema.ID, GetTypeName(Node{}))
 	ut.Equal(t, schema.PluralName, "nodes")
 	ut.Equal(t, schema.Version.Group, "testing")
 	ut.Equal(t, schema.Version.Path, "/v1")
-	ut.Equal(t, schema.Parent, "cluster")
+	ut.Equal(t, schema.Parent, GetTypeName(Cluster{}))
 	ut.Equal(t, schema.CollectionMethods, []string{"GET", "POST"})
 	ut.Equal(t, schema.ResourceMethods, []string{"GET", "DELETE", "PUT"})
 	ut.Equal(t, len(schema.ResourceFields), 3)
