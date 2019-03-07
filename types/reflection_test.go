e6599b924c83a1f71b4dca1c3c89de83585dfa2c
joe-yu <yuhaijiao@zdns.cn>
2019-03-07 02:18:58 +0000

    fix gen urlmethod and update uint test

--- types/reflection_test.go
+++ types/reflection_test.go
@@ -22,10 +22,45 @@ type Node struct {
 	Name string `singlecloud:"notnullable"`
 }
 
+type Namespace struct {
+	Resource
+	Name string `singlecloud:"notnullable"`
+}
+
+type Pod struct {
+	Resource
+	Name string `singlecloud:"notnullable"`
+}
+
+type Container struct {
+	Resource
+	Name string `singlecloud:"notnullable"`
+}
+
 func TestReflection(t *testing.T) {
 	schemas := NewSchemas()
+	schemas.MustImportAndCustomize(&version, Cluster{}, nil, func(schema *Schema, handler Handler) {
+		schema.CollectionMethods = []string{"GET", "POST"}
+		schema.ResourceMethods = []string{"GET", "DELETE", "PUT"}
+	})
+
 	schemas.MustImportAndCustomize(&version, Node{}, nil, func(schema *Schema, handler Handler) {
 		schema.Parent = GetResourceType(Cluster{})
+		schema.CollectionMethods = []string{"GET", "POST"}
+		schema.ResourceMethods = []string{"GET", "DELETE", "PUT"}
+	})
+	schemas.MustImportAndCustomize(&version, Namespace{}, nil, func(schema *Schema, handler Handler) {
+		schema.Parent = GetResourceType(Cluster{})
+		schema.CollectionMethods = []string{"GET", "POST"}
+		schema.ResourceMethods = []string{"GET", "DELETE", "PUT"}
+	})
+	schemas.MustImportAndCustomize(&version, Pod{}, nil, func(schema *Schema, handler Handler) {
+		schema.Parent = GetResourceType(Namespace{})
+		schema.CollectionMethods = []string{"GET", "POST"}
+		schema.ResourceMethods = []string{"GET", "DELETE", "PUT"}
+	})
+	schemas.MustImportAndCustomize(&version, Container{}, nil, func(schema *Schema, handler Handler) {
+		schema.Parent = GetResourceType(Pod{})
 		schema.CollectionMethods = []string{"GET", "POST"}
 		schema.ResourceMethods = []string{"GET", "DELETE", "PUT"}
 	})
@@ -43,5 +78,23 @@ func TestReflection(t *testing.T) {
 		if field.CodeName == "Name" {
 			ut.Equal(t, field.Nullable, false)
 		}
+	}
+
+	expectUrl := []string{
+		"/testing/v1/clusters",
+		"/testing/v1/clusters/:cluster_id",
+		"/testing/v1/clusters/:cluster_id/nodes",
+		"/testing/v1/clusters/:cluster_id/nodes/:node_id",
+		"/testing/v1/clusters/:cluster_id/namespaces",
+		"/testing/v1/clusters/:cluster_id/namespaces/:namespace_id",
+		"/testing/v1/clusters/:cluster_id/namespaces/:namespace_id/pods",
+		"/testing/v1/clusters/:cluster_id/namespaces/:namespace_id/pods/:pod_id",
+		"/testing/v1/clusters/:cluster_id/namespaces/:namespace_id/pods/:pod_id/containers",
+		"/testing/v1/clusters/:cluster_id/namespaces/:namespace_id/pods/:pod_id/containers/:container_id",
+	}
+	urlMethods := schemas.UrlMethods()
+	ut.Equal(t, len(urlMethods), len(expectUrl))
+	for _, url := range expectUrl {
+		ut.Equal(t, len(urlMethods[url]) != 0, true)
 	}
 }
