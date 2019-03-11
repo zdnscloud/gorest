181f23f245c605522b85e4883adbb93e2ae5295c
joe-yu <yuhaijiao@zdns.cn>
2019-03-11 08:37:12 +0000

    add get and list unit test

--- api/handler/request_test.go
+++ api/handler/request_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/base64"
 	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"reflect"
 	"testing"
 
@@ -34,4 +35,72 @@ func TestParseCreateBody(t *testing.T) {
 	ut.Equal(t, string(content), "testContent")
 	ut.Equal(t, obj.(*Foo).Type, "foo")
 	ut.Equal(t, obj.(*Foo).Name, "bar")
+}
+
+type Handler struct{}
+
+func (h *Handler) Create(obj types.Object, yamlContent []byte) (interface{}, *types.APIError) {
+	return nil, nil
+}
+
+func (h *Handler) Delete(obj types.Object) *types.APIError {
+	return nil
+}
+
+func (h *Handler) Update(obj types.Object) (interface{}, *types.APIError) {
+	return nil, nil
+}
+func (h *Handler) List(obj types.Object) interface{} {
+	return nil
+}
+
+func (h *Handler) Get(obj types.Object) interface{} {
+	return nil
+}
+
+func (h *Handler) Action(obj types.Object, action string, params map[string]interface{}) (interface{}, *types.APIError) {
+	return nil, nil
+}
+
+func TestList(t *testing.T) {
+	var noerr *types.APIError
+	req, _ := http.NewRequest("GET", "/test", nil)
+	req.Host = "127.0.0.1:1234"
+	apiContext := &types.APIContext{
+		Request:  req,
+		Response: httptest.NewRecorder(),
+		Schema: &types.Schema{
+			ID:        "foo",
+			StructVal: reflect.ValueOf(Foo{}),
+			Handler:   &Handler{},
+		},
+		ResponseFormat: "json",
+	}
+
+	expectCollection := "{\"type\":\"collection\",\"resourceType\":\"foo\",\"links\":{\"self\":\"http://127.0.0.1:1234/test\"},\"data\":[]}"
+	err := ListHandler(apiContext)
+	ut.Equal(t, err, noerr)
+	ut.Equal(t, apiContext.Response.(*httptest.ResponseRecorder).Code, http.StatusOK)
+	body := apiContext.Response.(*httptest.ResponseRecorder).Body
+	ut.Equal(t, body.String(), expectCollection)
+}
+
+func TestGet(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/test/123", nil)
+	req.Host = "127.0.0.1:1234"
+	apiContext := &types.APIContext{
+		ID:       "123",
+		Request:  req,
+		Response: httptest.NewRecorder(),
+		Schema: &types.Schema{
+			ID:        "foo",
+			StructVal: reflect.ValueOf(Foo{}),
+			Handler:   &Handler{},
+		},
+		ResponseFormat: "json",
+	}
+
+	expectResut := types.NewAPIError(types.NotFound, "no found foo with id 123")
+	err := ListHandler(apiContext)
+	ut.Equal(t, err, expectResut)
 }
