53fb708b94507f01762fd6396765699c61a38ffd
thesinglecloud <hanfeng@zdns.cn>
2019-09-24 02:57:42 +0000

    fix bug that when list handler return empty slice, colleciton will return nil

--- resource/collection_test.go
+++ resource/collection_test.go
@@ -17,4 +17,11 @@ func TestCollectionToJson(t *testing.T) {
 	ut.Assert(t, rs.Resources != nil, "")
 	d, _ := json.Marshal(rs)
 	ut.Equal(t, string(d), `{"type":"collection","data":[]}`)
+
+	rs2, err := NewResourceCollection(r, []*dumbResource{})
+	ut.Assert(t, err == nil, "")
+	ut.Assert(t, rs2.Resources != nil, "")
+	ut.Equal(t, len(rs2.Resources), 0)
+	d2, _ := json.Marshal(rs2)
+	ut.Equal(t, string(d), string(d2))
 }
