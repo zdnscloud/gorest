11bb03e46158d66515ba3c5475260eeb8c1c405e
yuchunyun <yuchunyun@zdns.cn>
2019-12-02 09:55:58 +0000

    add Filters for Context

--- resource/context.go
+++ resource/context.go
@@ -1,9 +1,30 @@
 package resource
 
 import (
-	"github.com/zdnscloud/gorest/error"
+	"fmt"
 	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/zdnscloud/gorest/error"
 )
+
+const (
+	Eq      Modifier = "eq"
+	Ne      Modifier = "ne"
+	Lt      Modifier = "lt"
+	Gt      Modifier = "gt"
+	Lte     Modifier = "lte"
+	Gte     Modifier = "gte"
+	Prefix  Modifier = "prefix"
+	Suffix  Modifier = "suffix"
+	Like    Modifier = "like"
+	NotLike Modifier = "notlike"
+	Null    Modifier = "null"
+	NotNull Modifier = "notnull"
+)
+
+type Modifier string
 
 type Context struct {
 	Schemas  SchemaManager
@@ -12,6 +33,13 @@ type Context struct {
 	Resource Resource
 	Method   string
 	params   map[string]interface{}
+	Filters  []Filter
+}
+
+type Filter struct {
+	Name string
+	Modifier
+	Value []string
 }
 
 func NewContext(resp http.ResponseWriter, req *http.Request, schemas SchemaManager) (*Context, *error.APIError) {
@@ -27,6 +55,7 @@ func NewContext(resp http.ResponseWriter, req *http.Request, schemas SchemaManag
 		Schemas:  schemas,
 		Method:   req.Method,
 		params:   make(map[string]interface{}),
+		Filters:  genFilters(req.URL),
 	}, nil
 }
 
@@ -37,4 +66,53 @@ func (ctx *Context) Set(key string, value interface{}) {
 func (ctx *Context) Get(key string) (interface{}, bool) {
 	v, ok := ctx.params[key]
 	return v, ok
+}
+
+func genFilters(url *url.URL) []Filter {
+	filters := make([]Filter, 0)
+	for k, v := range url.Query() {
+		var filter Filter
+		i := strings.LastIndexAny(k, "_")
+		if i < 0 {
+			filter.Name = k
+			filter.Modifier = Eq
+		} else {
+			filter.Name = k[:i]
+			filter.Modifier = VerifyModifier(k[i+1:])
+		}
+		filter.Value = v
+		filters = append(filters, filter)
+	}
+	return filters
+}
+
+func VerifyModifier(str string) Modifier {
+	switch str {
+	case "eq":
+		return Eq
+	case "ne":
+		return Ne
+	case "lt":
+		return Lt
+	case "gt":
+		return Gt
+	case "lte":
+		return Lte
+	case "gte":
+		return Gte
+	case "prefix":
+		return Prefix
+	case "suffix":
+		return Suffix
+	case "like":
+		return Like
+	case "notlike":
+		return NotLike
+	case "null":
+		return Null
+	case "notnull":
+		return NotNull
+	default:
+		return Modifier(fmt.Sprintf("Unkown(%s)", str))
+	}
 }
